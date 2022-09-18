05c637c1695677103cffb8611ba1d017d85d04ef
Vipinnnn <2019vipin.talreja@ves.ac.in>
2022-09-18 14:22:22 +0530

    Day1

---
+++ Day1/main.py
@@ -0,0 +1,5 @@
+print("Welcome to band name generator")
+cityName = input("What's the name of city you grew up in? ")
+petName = input("What's your pet's name? ")
+
+print("Your band name is " + cityName + " " + petName)
--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
 Python 100 Days of Code:
+Day1:
+Print function
+String concatenation
+Input function
+Variables, Naming variables
+
+Day1 project: Band Name Generator, using simple string concatenation.
